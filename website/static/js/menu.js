3ec63ddb6b914bfec55dfde5c3f0b26e94f26f76
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-08-02 14:50:04 +0200

    Fix left menu behavior

---
+++ website/static/js/menu.js
@@ -0,0 +1,27 @@
+window.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    const subMenu = document.querySelectorAll(".subNavGroup");
+
+    Object.entries(subMenu).map(object => {
+      const listElement = object[1].querySelectorAll("ul li");
+      Object.entries(listElement).map(item => {
+        if (item[1].classList.contains("navListItemActive")) {
+          item[1].parentElement.parentElement.classList.add("active");
+        }
+      });
+
+      object[1].querySelector("h4").addEventListener(
+        "click",
+        function(e) {
+          // Object.entries(subMenu).map(item => {
+          //   item[1].classList.remove("active");
+          // });
+          object[1].classList.toggle("active");
+        },
+        false
+      );
+    });
+  },
+  false
+);
