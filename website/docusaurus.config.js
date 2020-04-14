f07839fd7efeae2fe26428d364ad51c91c886609
Patryk Zawadzki <patrys@room-303.com>
2020-04-14 18:29:07 +0200

    Add footer links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,25 +15,20 @@ module.exports = {
     algolia: {
       apiKey: "33843c1b27663cc760ddc60811ba51d7",
       indexName: "saleor",
-      placeholder: "Search Saleor Documentation"
+      placeholder: "Search Saleor Documentation",
     },
-    /* path to images for header/footer */
-    twitterIcon: "img/twitter-icon.svg",
-    fbIcon: "img/facebook-icon.svg",
-    soIcon: "img/stackoverflow-icon.svg",
-    githubIcon: "img/github-icon.svg",
 
     /* Colors for website */
     colors: {
       primaryColor: "#0c7d7b",
-      secondaryColor: "#5d623c"
+      secondaryColor: "#5d623c",
     },
 
     navbar: {
       hideOnScroll: true,
       logo: {
         alt: "Saleor",
-        src: "img/saleor-logo.svg"
+        src: "img/saleor-logo.svg",
       },
       links: [
         {
@@ -41,28 +36,74 @@ module.exports = {
           label: "versions",
           position: "right",
           style: {
-            fontVariant: "small-caps"
-          }
+            fontVariant: "small-caps",
+          },
         },
         {
           to: "docs/dashboard/before-you-start",
-          label: "Dashboard Manual"
+          label: "Dashboard Manual",
         },
         { to: "docs/api/intro", label: "GraphQL API" },
         {
           to: "docs/getting-started/intro",
-          label: "Developer's Guide"
-        }
-      ]
+          label: "Developer's Guide",
+        },
+      ],
     },
 
     footer: {
       // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-      copyright: `Copyright © ${new Date().getFullYear()} Mirumee Software`,
+      copyright: `Copyright © 2009–${new Date().getFullYear()} <a href="https://mirumee.com/">Mirumee Software</a>`,
       logo: {
         src: "img/footer-logo.png",
-        alt: "Saleor"
-      }
+        alt: "Saleor",
+      },
+      links: [
+        {
+          title: "Saleor Commerce",
+          items: [
+            { href: "https://saleor.io", label: "Home" },
+            {
+              href: "https://saleor.io/terms-and-conditions/",
+              label: "Terms of use",
+            },
+            {
+              href: "https://saleor.io/terms-and-conditions/#privacy",
+              label: "Privacy policy",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              href: "https://spectrum.chat/saleor",
+              label: "Spectrum",
+            },
+            {
+              href: "https://stackoverflow.com/questions/tagged/saleor",
+              label: "Stack Overflow",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              href: "https://saleor.io/blog/",
+              label: "Blog",
+            },
+            {
+              href: "https://github.com/mirumee/saleor/",
+              label: "GitHub",
+            },
+            {
+              href: "https://twitter.com/getsaleor",
+              label: "Twitter",
+            },
+          ],
+        },
+      ],
     },
 
     // Open Graph and Twitter card images.
@@ -74,15 +115,9 @@ module.exports = {
 
     prism: {
       theme: require("prism-react-renderer/themes/github"),
-      darkTheme: require("prism-react-renderer/themes/dracula")
-    }
+      darkTheme: require("prism-react-renderer/themes/dracula"),
+    },
   },
-
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: [
-    "https://buttons.github.io/buttons.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"
-  ],
 
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
@@ -98,16 +133,16 @@ module.exports = {
       "@docusaurus/preset-classic",
       {
         theme: {
-          customCss: require.resolve("./src/css/theme.css")
+          customCss: require.resolve("./src/css/theme.css"),
         },
         docs: {
           // docs folder path relative to website dir.
           path: "../docs",
           editUrl: "https://github.com/mirumee/saleor-docs/edit/master/docs/",
           // sidebars file relative to website dir.
-          sidebarPath: require.resolve("./sidebars.json")
-        }
-      }
-    ]
-  ]
+          sidebarPath: require.resolve("./sidebars.json"),
+        },
+      },
+    ],
+  ],
 };
