151eec4be26cf7d99bfa6f1adaae40461793573e
Patryk Zawadzki <patrys@room-303.com>
2020-04-21 12:21:57 +0200

    Update icons, redirect from empty index

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -6,42 +6,10 @@
  */
 
 const React = require("react");
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-
-import Layout from "@theme/Layout";
+import { Redirect } from "@docusaurus/router";
 
 const Index = () => {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
-
-  return (
-    <Layout
-      permalink="/"
-      title={siteConfig.title}
-      description={siteConfig.tagLine}
-    >
-      <div className="docMainWrapper wrapper">
-        <div className="container mainContainer">
-          <div className="wrapper">
-            <div className="post">
-              <header className="postHeader">
-                <h1 className="postHeaderTitle">
-                  Welcome to the Saleor documentation!
-                </h1>
-              </header>
-              <article>
-                <Link to={useBaseUrl("/docs/index")}>
-                  Go to documentation contents.
-                </Link>
-              </article>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+  return <Redirect to="/docs/index/" />;
 };
 
 export default Index;
