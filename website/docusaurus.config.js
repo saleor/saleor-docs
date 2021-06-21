1d539bd60f4dc8e5eb38ff316fc090a18178a4db
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-06-21 13:30:09 +0200

    Add esbuild

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,6 +25,17 @@ module.exports = {
     ],
   ],
 
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        format: isServer ? 'cjs' : undefined,
+        target: isServer ? 'node12' : 'es2017',
+      },
+    }),
+  },
+
   themeConfig: {
     algolia: {
       apiKey: "33843c1b27663cc760ddc60811ba51d7",
