ec32b3787e856ae5c73881ba56d6c250fbf56eda
Maciej Korycinski <maciej@mirumee.com>
2022-02-03 11:54:43 +0100

    Add basic explanaition of app's extensions (#348)
    * Add basic explanaition of app's extensions
    
    * Update a table of mounting places. apply review comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Karol <57907951+karolkielecki@users.noreply.github.com>
    
    Co-authored-by: Karol <57907951+karolkielecki@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -125,6 +125,7 @@ module.exports = {
               "developer/extending/apps/installing-apps",
               "developer/extending/apps/asynchronous-webhooks",
               "developer/extending/apps/synchronous-webhooks",
+              "developer/extending/apps/extensions",
               "developer/extending/apps/sample-webhook-payloads",
             ],
           },
