5c874d17a7788c0dadcd3d9b71f42e14e8be6ccb
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2022-08-24 13:12:00 +0200

    feat: :sparkles: initialize Sentry in Layout/index.js

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import Layout from "@theme-original/Layout";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import siteConfig from "@generated/docusaurus.config";
+
+const sentryDSN = siteConfig.customFields.sentryDSN;
+
+if (sentryDSN) {
+  Sentry.init({
+    dsn: sentryDSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
+
 export default function LayoutWrapper(props) {
   return (
     <>
