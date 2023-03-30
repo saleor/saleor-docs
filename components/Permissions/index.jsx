d40b5026209717dbc82d9ad0760007699ef1b710
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-03-30 08:34:50 +0200

    Badge styles

--- components/Permissions/index.jsx
+++ components/Permissions/index.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css";
 
 const Permissions = ({ permissions = "" }) => {
   const permissionList = permissions.split(",");
-  const suffix = permissionList.length > 1 ? " permissions: " : " permission: ";
+  const suffix = permissionList.length > 1 ? "permissions." : "permission.";
 
   return (
     <div className={`margin-bottom--sm ` + styles.permissions}>
@@ -16,21 +16,22 @@ const Permissions = ({ permissions = "" }) => {
           width="24"
         >
           <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z" />
+          <path
+            fill="currentColor"
+            d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"
+          />
         </svg>
       </div>
 
       <div className="margin-left--sm">
         <div>
-          Requires the following
-          {suffix}
-        </div>
-        <div>
-          {permissionList.map((permission) => (
-            <span class="badge badge--danger margin-right--sm">
+          {"Requires "}
+          {permissionList.map((permission, idx) => (
+            <span key={idx} className="badge badge--danger margin-right--sm">
               {permission}
             </span>
           ))}
+          {suffix}
         </div>
       </div>
     </div>
