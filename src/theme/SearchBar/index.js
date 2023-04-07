c0ad8320f306d9eef83e8ccd3aa1f727876e1c15
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-04-07 17:08:58 +0200

    Ask AI

---
+++ src/theme/SearchBar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "@theme-original/SearchBar";
+import AskAI from "@site/components/AskAI";
+
+export default function SearchBarWrapper(props) {
+  return (
+    <>
+      <span style={{ display: "flex" }}>
+        <AskAI />
+        <SearchBar {...props} />
+      </span>
+    </>
+  );
+}
