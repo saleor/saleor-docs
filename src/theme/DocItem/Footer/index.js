762294994a2142999c5083734b00492c33667deb
timur <timuric@gmail.com>
2023-04-11 14:39:11 +0200

    Add radix color theme switch

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "@theme-original/DocItem/Footer";
 import Feedback from "@site/components/Feedback";
+import { useColorMode } from "@docusaurus/theme-common";
+
+const useRadixTheme = () => {
+  const { colorMode } = useColorMode();
+
+  //Radix color variables require a class on the root element
+  useEffect(() => {
+    //Add classes for radix color variables
+    if (colorMode === "dark") {
+      document.documentElement.classList.add("dark-theme");
+    } else {
+      document.documentElement.classList.remove("dark-theme");
+    }
+  }, [colorMode]);
+};
 
 export default function FooterWrapper(props) {
+  //This hook can be added in any component that has access to the colorMode, at the time of writing this is the only ejected component
+  useRadixTheme();
   return (
     <>
       <Feedback />
