602cd83c8599c519dedc0fac044646a634743e10
timur <timuric@gmail.com>
2023-04-19 16:46:54 +0200

    Remove radix theme

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,23 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import SearchBar from "@theme-original/SearchBar";
 import AskAI from "@site/components/AskAI";
-import { useColorMode } from "@docusaurus/theme-common";
-
-const useRadixTheme = () => {
-  const { colorMode } = useColorMode();
-
-  //Radix color variables require a class on the root element
-  useEffect(() => {
-    //Add classes for radix color variables
-    if (colorMode === "dark") {
-      document.documentElement.classList.add("dark-theme");
-    } else {
-      document.documentElement.classList.remove("dark-theme");
-    }
-  }, [colorMode]);
-};
 export default function SearchBarWrapper(props) {
-  useRadixTheme();
   return (
     <>
       <span style={{ display: "flex" }}>
