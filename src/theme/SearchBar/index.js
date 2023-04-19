ec830a1770fe0354a6419fc90d8cb52ec022e97d
timur <timuric@gmail.com>
2023-04-19 13:49:24 +0200

    Move radix theme switch to search

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "@theme-original/SearchBar";
 import AskAI from "@site/components/AskAI";
+import { useColorMode } from "@docusaurus/theme-common";
 
+const useRadixTheme = () => {
+  const { colorMode } = useColorMode();
+
+  //Radix color variables require a class on the root element
+  useEffect(() => {
+    //Add classes for radix color variables
+    if (colorMode === "dark") {
+      document.documentElement.classList.add("dark-theme");
+    } else {
+      document.documentElement.classList.remove("dark-theme");
+    }
+  }, [colorMode]);
+};
 export default function SearchBarWrapper(props) {
+  useRadixTheme();
   return (
     <>
       <span style={{ display: "flex" }}>
