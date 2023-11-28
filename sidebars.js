98cfe1df0888ece0c3baa1d8df2409b64e884669
timur <timuric@gmail.com>
2023-11-28 19:18:31 +0100

    Remove features page

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,6 @@ module.exports = {
       Overview: [
         "overview/introduction",
         "overview/architecture",
-        "overview/features",
         "overview/quickstart",
       ],
     },
