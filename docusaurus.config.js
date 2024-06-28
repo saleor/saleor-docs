d531ab1d7cd63297698f496bfe57fcf3a1bdc453
Bartłomiej Wiaduch <tukan2can@gmail.com>
2024-06-28 17:28:47 +0200

    Remove segment script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -249,10 +249,4 @@ module.exports = {
   customFields: {
     sentryDSN: process.env.SENTRY_DSN,
   },
-  scripts: [
-    {
-      src: "https://cdn.segment.com/analytics.js/v1/JlfojBlUjPv2SgxOBkDAwpmshEaC97t4/analytics.min.js",
-      async: true,
-    },
-  ],
 };
