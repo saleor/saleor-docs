18cf2f7b90b3e6e4c97622e6ff9a44b9733587b8
Ania Warzecha <anna.warzecha@saleor.io>
2024-07-01 16:52:59 +0200

    Change edit link for current branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -229,7 +229,7 @@ module.exports = {
           },
           editUrl: function ({ version, versionDocsDirPath, docPath }) {
             if (version === "current") {
-              return `https://github.com/saleor/saleor-docs/edit/main/docs/${docPath}`;
+              return `https://github.com/saleor/saleor-docs/edit/3.19/docs/${docPath}`;
             } else {
               return `https://github.com/saleor/saleor-docs/edit/main/${versionDocsDirPath}/${docPath}`;
             }
