ae74bfca8116c90d0579858a1d9a8bed1b08f1a5
timur <timuric@gmail.com>
2024-07-10 10:58:08 +0200

    Nudge details

--- components/CardGrid/index.jsx
+++ components/CardGrid/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const CardGrid = ({ children, home = false }) => (
-  <section className={"row " + styles.cardGrid}>
+  <section className={styles.cardGrid}>
     {"map" in children ? (
       children.map((child, idx) => <article key={idx}>{child}</article>)
     ) : (
