a6476f8cfa8ba7bb79ca7854839ddc83c0bbeb5c
timur <timuric@gmail.com>
2024-07-10 16:09:52 +0200

    Fix links

--- components/Apps/Apps.jsx
+++ components/Apps/Apps.jsx
@@ -31,48 +31,95 @@ const Apps = () => {
   return (
     <div className={s.root}>
       <div className={s.container}>
-        <Shell color={"rgb(10, 191, 83)"} title={"Adyen"}>
+        <Shell
+          color={"rgb(10, 191, 83)"}
+          title={"Adyen"}
+          href={"developer/app-store/apps/adyen/overview"}
+        >
           <Adyen />
         </Shell>
-        <Shell color={"rgb(223, 110, 44)"} title={"AvaTax"}>
+        <Shell
+          color={"rgb(223, 110, 44)"}
+          title={"Avatax"}
+          href={"developer/app-store/apps/avatax/overview"}
+        >
           <Taxes />
         </Shell>
-        <Shell color={"rgb(39, 180, 118)"} title={"CMS"}>
+        <Shell
+          color={"rgb(39, 180, 118)"}
+          title={"CMS"}
+          href={"developer/app-store/apps/cms"}
+        >
           <CMS />
         </Shell>
-        <Shell color={"rgb(137, 82, 255)"} title={"CRM"}>
+        <Shell
+          color={"rgb(137, 82, 255)"}
+          title={"CRM"}
+          href={"developer/app-store/apps/crm"}
+        >
           <CRM />
         </Shell>
-        <Shell color={"rgb(59, 213, 121)"} title={"Data Importer"}>
-          <Data />
-        </Shell>
-        <Shell color={"rgb(227, 149, 60)"} title={"Invoices"}>
+        {/*missing docs*/}
+        {/*<Shell color={"rgb(59, 213, 121)"} title={"Data importer"}>*/}
+        {/*  <Data />*/}
+        {/*</Shell>*/}
+        <Shell
+          color={"rgb(227, 149, 60)"}
+          title={"Invoices"}
+          href={"developer/app-store/apps/invoices"}
+        >
           <Invoices />
         </Shell>
-        <Shell color={"rgb(58, 86, 199)"} title={"Klaviyo"}>
-          <Placeholder />
-        </Shell>{" "}
-        <Shell color={"rgb(122, 150, 176)"} title={"Product Feed"}>
+
+        {/*missing docs*/}
+        {/*<Shell color={"rgb(58, 86, 199)"} title={"Klaviyo"}>*/}
+        {/*  <Placeholder />*/}
+        {/*</Shell>{" "}*/}
+
+        <Shell
+          color={"rgb(122, 150, 176)"}
+          title={"Product Feed"}
+          href={"developer/app-store/apps/product-feed"}
+        >
           <ProductFeed />
         </Shell>
-        <Shell color={"rgb(199, 58, 63)"} title={"Search"}>
+        <Shell
+          color={"rgb(199, 58, 63)"}
+          title={"Search"}
+          href={"developer/app-store/apps/search"}
+        >
           <Search />
         </Shell>
-        <Shell color={"rgb(37, 195, 101)"} title={"Twilio Segment"}>
+        <Shell
+          color={"rgb(37, 195, 101)"}
+          title={"Twilio Segment"}
+          href={"developer/app-store/apps/segment"}
+        >
           <Segment />
         </Shell>
-        <Shell color={"rgb(95, 58, 199)"} title={"Slack"}>
-          <Placeholder />
-        </Shell>
-        <Shell color={"rgb(199, 159, 58)"} title={"SMTP"}>
+
+        {/*missing docs*/}
+        {/*<Shell color={"rgb(95, 58, 199)"} title={"Slack"}>*/}
+        {/*  <Placeholder />*/}
+        {/*</Shell>*/}
+
+        <Shell
+          color={"rgb(199, 159, 58)"}
+          title={"SMTP"}
+          href={"developer/app-store/apps/smtp/overview"}
+        >
           <Notification />
         </Shell>
-        <Shell color={"rgb(99, 91, 255)"} title={"Stripe"}>
-          <Stripe />
-        </Shell>
-        <Shell color={"rgb(86, 189, 104)"} title={"TaxJar"}>
-          <TaxJar />
-        </Shell>
+
+        {/*missing docs*/}
+        {/*<Shell color={"rgb(99, 91, 255)"} title={"Stipe"}>*/}
+        {/*  <Stripe />*/}
+        {/*</Shell>*/}
+
+        {/*missing docs*/}
+        {/*<Shell color={"rgb(86, 189, 104)"} title={"Taxjar"}>*/}
+        {/*  <TaxJar />*/}
+        {/*</Shell>*/}
       </div>
     </div>
   );
