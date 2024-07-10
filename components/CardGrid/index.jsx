82c1d0d9327184075d5dc9992f63cffddd3ecea1
timur <timuric@gmail.com>
2024-07-10 12:55:50 +0200

    Add container query

--- components/CardGrid/index.jsx
+++ components/CardGrid/index.jsx
@@ -4,11 +4,13 @@ import styles from "./styles.module.css";
 
 const CardGrid = ({ children, home = false }) => (
   <section className={styles.cardGrid}>
-    {"map" in children ? (
-      children.map((child, idx) => <article key={idx}>{child}</article>)
-    ) : (
-      <article>{children}</article>
-    )}
+    <div className={styles.container}>
+      {"map" in children ? (
+        children.map((child, idx) => <article key={idx}>{child}</article>)
+      ) : (
+        <article>{children}</article>
+      )}
+    </div>
   </section>
 );
 
