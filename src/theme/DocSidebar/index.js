9ed5361ba7b0f1f58d653bef2ffc3d8138591b8f
timur <timuric@gmail.com>
2024-07-22 13:22:20 +0200

    Scroll to chapter in sidebar

---
+++ src/theme/DocSidebar/index.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import DocSidebar from "@theme-original/DocSidebar";
+
+export default function DocSidebarWrapper(props) {
+  const { path, sidebar } = props;
+
+  useEffect(() => {
+    const activeItem = sidebar.map((x) => x.href).indexOf(path);
+
+    if (activeItem < 0) {
+      return;
+    }
+
+    const parentTitle = sidebar
+      .slice(0, activeItem)
+      .reverse()
+      .find((item) => item.className === "menu__group-label");
+
+    if (!parentTitle) {
+      return;
+    }
+
+    const el = Array.from(document.querySelectorAll(".menu__group-label")).find(
+      (el) => {
+        return (
+          el.innerText.toLocaleLowerCase() === parentTitle.value.toLowerCase()
+        );
+      }
+    );
+
+    if (!el) return;
+
+    const menu = document.querySelector("nav.menu");
+
+    const selectedYPos = el.getBoundingClientRect().y;
+
+    const TOP_OFFSET = 100;
+
+    const t = setTimeout(() => {
+      menu.scrollTo({
+        top: menu.scrollTop + selectedYPos - TOP_OFFSET,
+        behavior: "smooth",
+      });
+    });
+
+    return () => {
+      clearTimeout(t);
+    };
+  }, []);
+
+  return (
+    <>
+      <DocSidebar {...props} />
+    </>
+  );
+}
