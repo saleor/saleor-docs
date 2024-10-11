122d63e54ef490f53fb6b1d865c9ca5d2eeea5df
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-10-11 09:20:30 +0200

    Remove sentry

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,20 +1,6 @@
 import ErrorBoundary from "@docusaurus/ErrorBoundary";
-import siteConfig from "@generated/docusaurus.config";
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 import Layout from "@theme-original/Layout";
 import React from "react";
-
-const sentryDSN = siteConfig.customFields.sentryDSN;
-
-if (sentryDSN) {
-  Sentry.init({
-    dsn: sentryDSN,
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 0,
-    enableTracing: false,
-  });
-}
 
 export default function LayoutWrapper(props) {
   return (
