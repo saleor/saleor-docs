88e55c5a4e668dd1a98121139b8be1209dd54923
Ania Warzecha <anna.warzecha@saleor.io>
2024-10-11 12:59:38 +0200

    Try out PWA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,34 @@ module.exports = {
         },
       },
     ],
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json", // your PWA manifest
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+        ],
+      },
+    ],
   ],
 
   webpack: {
